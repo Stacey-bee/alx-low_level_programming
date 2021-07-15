f19aabc7eb6dbad612dc20b21895d28674b40433
Stacey-bee <staceynakanwagi@gmail.com>
2021-07-15 10:46:45 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,44 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
- * string_nconcat - concatenate two strings
- * @s1: the first string
- * @s2: the second string
- * @n: the maximum number of bytes to use from s2
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
  *
- * Description: Copy s1 followed by the first n bytes of s2 into dynamically
- * allocated memory, and append a terminating null-byte. If n is greater than
- * or equal to the length of the string s2, use only the bytes from s2 up to
- * it's terminating null-byte. Treat NULL as an an empty string.
- *
- * Return: If memory allocation fails, return NULL. Otherwise, return a pointer
- * to the first character of the new string.
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *cat;
-	unsigned int s1_len = 0;
-	unsigned int s2_len = 0;
+	char *concat;
+	unsigned int len = n, index;
 
-	if (s1 != NULL)
-	{
-		while (s1[s1_len])
-			++s1_len;
-	}
+	if (s1 == NULL)
+		s1 = "";
 
-	if (s2 != NULL)
-	{
-		while (s2_len < n && s2[s2_len])
-			++s2_len;
-	}
+	if (s2 == NULL)
+		s2 = "";
 
-	cat = malloc(sizeof(char) * s1_len + s2_len + 1);
-	if (cat == NULL)
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
 
-	for (n = 0; n < s1_len; ++n, ++cat)
-		*cat = s1[n];
+	len = 0;
 
-	for (n = 0; n < s2_len; ++n, ++cat)
-		*cat = s2[n];
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
 
-	*cat = '\0';
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
 
-	return (cat - s1_len - s2_len);
+	concat[len] = '\0';
+
+	return (concat);
 }
-
