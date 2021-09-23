ac749b91519297ac37b52d9eefc4e8075855453b
Stacey-bee <staceynakanwagi@gmail.com>
2021-09-23 05:49:21 -0700

    modifying indentation to suit betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,13 @@
 
 /**
  * main - printing a string
- * Description: using the printf function to print a string followed by a new line
+ * Description: using the printf function to print a string
+ * followed by a new line
  * Return: 0
  */
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
